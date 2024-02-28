cae57ecbc384fa2ca5b03eb18b788f35273c340a
kayfernander2022 <kayfernander@hotmail.com>
2024-02-28 12:03:18 -0500

    tshirt controller complete routes added

--- controllers/tshirt.js
+++ controllers/tshirt.js
@@ -14,5 +14,42 @@ router.get('/', async(req,res)=>{
   }
 })
 
+//Delete
+router.delete('/:id', async(req,res)=>{
+  try{
+    res.json(await Hair.findByIdAndDelete(req.params.id))
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
+
+//Update
+router.put('/:id', async(req,res)=>{
+  try{
+    res.json(await Hair.findByIdAndUpdate(req.params.id,req.body,{new:true}))
+  }catch(error){
+
+    console.log(error)
+    res.status(400).json(error)
+  }
+})
+
+//Create
+router.post('/', async(req,res)=>{
+  try{
+    res.json(await Hair.create(req.body))
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
+
+//Show
+router.get('/:id', async(req,res)=>{
+  try{
+    res.json(await Hair.findById(req.params.id))
+  }catch(error){
+    res.status(400).json(error)
+  }
+})
 
 module.exports = router
